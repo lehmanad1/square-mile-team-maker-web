4812eebefc58dc53d4a85ba208ca5ffb692ec095
Andrew Lehman <lehmanad1@live.com>
2025-05-31 22:08:52 -0400

    updating vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-  },
+  base: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html'),
+      },
+    },
   },
 });
